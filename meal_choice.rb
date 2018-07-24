f158f468911141bea468f7a8c20ea9aadfbd7a7a
Sarah Silverman <ssilverman1001@gmail.com>
2018-07-24 15:29:33 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -32,6 +32,6 @@ snacks
 puts snacks
 ## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
 def food (snacks= "Cheetos")
-  "Any time, is the right time for #{snacks}"
+  puts "Any time, is the right time for #{snacks}"
 end
 food 
